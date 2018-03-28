9f4d217ae58772d1a980d542f9c35a749bde03fc
forenheith <6537537@gmail.com>
2018-03-29 00:00:38 +0300

    Done

---
+++ ExpVisitor.cs
@@ -0,0 +1,23 @@
+﻿// 2018032823:41ExpVisitor.csРоман Евсеев2018032823:41
+
+using System.Linq.Expressions;
+
+namespace ExpressionTree1
+{
+    public class ExpVisitor : ExpressionVisitor
+    {
+       public Expression ConverTo(BinaryExpression binaryExpression)
+        {
+            if (binaryExpression.NodeType.Equals(ExpressionType.Add))
+            {
+                var left = this.Visit(binaryExpression.Left);
+                return Expression.MakeUnary(ExpressionType.Increment, left, typeof(int));
+            }
+            if (!binaryExpression.NodeType.Equals(ExpressionType.Subtract)) return null;
+            {
+                var left = this.Visit(binaryExpression.Left);
+                return Expression.MakeUnary(ExpressionType.Decrement, left, typeof(int));
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,15 +1,23 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Linq.Expressions;
 
 namespace ExpressionTree1
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
+            var addExp = Expression.Add(Expression.Variable(typeof(int), "5"), Expression.Constant(1));
+            var subExp = Expression.Subtract(Expression.Variable(typeof(int), "5"), Expression.Constant(1));
+            var divExp = Expression.Divide(addExp, subExp);
+            Console.WriteLine($"Initial state: {divExp}");
+
+            var convertedAdd = new ExpVisitor().ConverTo(addExp);
+            var convertedSub = new ExpVisitor().ConverTo(subExp);
+            divExp = Expression.Divide(convertedAdd, convertedSub);
+            Console.WriteLine($"Converted state: {divExp}");
+
+            Console.ReadKey();
         }
     }
 }
