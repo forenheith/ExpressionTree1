72dbd7c94f7b26ddefab0e7a059a9210cf844bd0
forenheith <forenheith@gmail.com>
2018-04-02 01:25:46 +0300

    correct visitor

--- ExpVisitor.cs
+++ ExpVisitor.cs
@@ -6,18 +6,50 @@ namespace ExpressionTree1
 {
     public class ExpVisitor : ExpressionVisitor
     {
-       public Expression ConverTo(BinaryExpression binaryExpression)
+        public Expression Visit(BinaryExpression binaryExpression)
         {
-            if (binaryExpression.NodeType.Equals(ExpressionType.Add))
+            return VisitBinary(binaryExpression);
+        }
+
+        protected override Expression VisitBinary(BinaryExpression node)
+        {
+            if (node.Equals(null))
+                return base.VisitBinary(node);
+
+            if (!node.NodeType.Equals(ExpressionType.Add) || !node.NodeType.Equals(ExpressionType.Subtract))
+                return base.VisitBinary(node);
+
+            ConstantExpression constant = null;
+            if (node.Left.NodeType.Equals(ExpressionType.Constant) ||
+                node.Right.NodeType.Equals(ExpressionType.Constant))
             {
-                var left = this.Visit(binaryExpression.Left);
-                return Expression.MakeUnary(ExpressionType.Increment, left, typeof(int));
+                constant = node.Left.NodeType.Equals(ExpressionType.Constant)
+                    ? (ConstantExpression) node.Left
+                    : (ConstantExpression) node.Right;
             }
-            if (!binaryExpression.NodeType.Equals(ExpressionType.Subtract)) return null;
+
+            ParameterExpression parameter = null;
+            if (node.Left.NodeType.Equals(ExpressionType.Parameter) ||
+                node.Right.NodeType.Equals(ExpressionType.Parameter))
             {
-                var left = this.Visit(binaryExpression.Left);
-                return Expression.MakeUnary(ExpressionType.Decrement, left, typeof(int));
+                parameter = node.Left.NodeType.Equals(ExpressionType.Parameter)
+                    ? (ParameterExpression) node.Left
+                    : (ParameterExpression) node.Right;
             }
+            if (!NeedToTransform(parameter, constant))
+                return base.VisitBinary(node);
+
+            return node.NodeType == ExpressionType.Add ? Expression.Increment(node) : Expression.Decrement(node);
+        }
+
+        private static bool NeedToTransform(ParameterExpression parameter, ConstantExpression constant)
+        {
+            if (parameter != null && constant != null)
+            {
+                return (parameter.Name.Equals("1") || constant.Value.Equals(1));
+            }
+
+            return parameter?.Name.Equals("1") ?? (constant?.Value.Equals(1) ?? false);
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -7,15 +7,13 @@ namespace ExpressionTree1
     {
         private static void Main(string[] args)
         {
-            var addExp = Expression.Add(Expression.Variable(typeof(int), "5"), Expression.Constant(1));
-            var subExp = Expression.Subtract(Expression.Variable(typeof(int), "5"), Expression.Constant(1));
+            var addExp = Expression.Add(Expression.Variable(typeof(int), "17"), Expression.Constant(1));
+            var subExp = Expression.Subtract(Expression.Variable(typeof(int), "7"), Expression.Constant(1));
             var divExp = Expression.Divide(addExp, subExp);
             Console.WriteLine($"Initial state: {divExp}");
 
-            var convertedAdd = new ExpVisitor().ConverTo(addExp);
-            var convertedSub = new ExpVisitor().ConverTo(subExp);
-            divExp = Expression.Divide(convertedAdd, convertedSub);
-            Console.WriteLine($"Converted state: {divExp}");
+            var convertedDiv = new ExpVisitor().Visit(divExp);
+            Console.WriteLine($"Converted state: {convertedDiv}");
 
             Console.ReadKey();
         }
