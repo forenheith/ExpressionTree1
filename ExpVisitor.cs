e128eb11ec1f0dd965a051f90989f2dc9bb290c9
MINSK\Raman_Yauseyeu <raman_yauseyeu@epam.com>
2018-04-03 18:01:30 +0300

    no message

--- ExpVisitor.cs
+++ ExpVisitor.cs
@@ -2,69 +2,68 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Linq.Expressions;
-using System.Xml;
 
 namespace ExpressionTree1
 {
     public class ExpVisitor : ExpressionVisitor
     {
-        //protected override Expression VisitBinary(BinaryExpression node)
-        //{
-        //    if (node.Equals(null))
-        //        return base.VisitBinary(node);
+        private readonly Dictionary<string, object> values =
+            new Dictionary<string, object> {{"a", 12}, {"b", 7}};
 
-        //    if (!node.NodeType.Equals(ExpressionType.Add) && !node.NodeType.Equals(ExpressionType.Subtract))
-        //        return base.VisitBinary(node);
+        protected override Expression VisitBinary(BinaryExpression node)
+        {
+            if (node.Equals(null))
+                return base.VisitBinary(node);
 
-        //    ConstantExpression constant = null;
-        //    if (node.Left.NodeType.Equals(ExpressionType.Constant) ||
-        //        node.Right.NodeType.Equals(ExpressionType.Constant))
-        //    {
-        //        constant = node.Left.NodeType.Equals(ExpressionType.Constant)
-        //            ? (ConstantExpression) node.Left
-        //            : (ConstantExpression) node.Right;
-        //    }
+            var isAddOrSub = !node.NodeType.Equals(ExpressionType.Add) &&
+                             !node.NodeType.Equals(ExpressionType.Subtract);
 
-        //    ParameterExpression parameter = null;
-        //    if (node.Left.NodeType.Equals(ExpressionType.Parameter) ||
-        //        node.Right.NodeType.Equals(ExpressionType.Parameter))
-        //    {
-        //        parameter = node.Left.NodeType.Equals(ExpressionType.Parameter)
-        //            ? (ParameterExpression) node.Left
-        //            : (ParameterExpression) node.Right;
-        //    }
+            ConstantExpression constant = null;
+            if (isAddOrSub)
+            {
+                if (node.Left.NodeType.Equals(ExpressionType.Constant) ||
+                    node.Right.NodeType.Equals(ExpressionType.Constant))
+                    constant = node.Left.NodeType.Equals(ExpressionType.Constant)
+                        ? (ConstantExpression) node.Left
+                        : (ConstantExpression) node.Right;
 
-        //    if (!NeedToTransform(parameter, constant))
-        //        return base.VisitBinary(node);
+                ParameterExpression parameter = null;
+                if (node.Left.NodeType.Equals(ExpressionType.Parameter) ||
+                    node.Right.NodeType.Equals(ExpressionType.Parameter))
+                    parameter = node.Left.NodeType.Equals(ExpressionType.Parameter)
+                        ? (ParameterExpression) node.Left
+                        : (ParameterExpression) node.Right;
 
-        //    return node.NodeType == ExpressionType.Add ? Expression.Increment(node) : Expression.Decrement(node);
-        //}
+                if (!NeedToTransform(parameter, constant))
+                    return base.VisitBinary(node);
+
+                return node.NodeType == ExpressionType.Add ? Expression.Increment(node) : Expression.Decrement(node);
+            }
+
+            return base.VisitBinary(node);
+        }
 
         private static bool NeedToTransform(ParameterExpression parameter, ConstantExpression constant)
         {
-            if (parameter != null && constant != null)
-            {
-                return (parameter.Name.Equals("1") || constant.Value.Equals(1));
-            }
+            if (parameter != null && constant != null) return parameter.Name.Equals("1") || constant.Value.Equals(1);
 
             return parameter?.Name.Equals("1") ?? (constant?.Value.Equals(1) ?? false);
         }
 
-        public Expression VLambda()
-        {
-            Expression<Func<int, int>> exp = (a) => a + 5;
-            return VisitLambda(exp);
-        }
 
         protected override Expression VisitLambda<T>(Expression<T> node)
         {
-            //var exp = Visit(node);
-            return Expression.Lambda(Visit(node.Body));
+            return node.Parameters.Any(p => values.ContainsKey(p.Name))
+                ? Visit(Expression.Lambda(node.Body, node.Parameters.Where(p => !values.ContainsKey(p.Name))))
+                : base.VisitLambda(node);
         }
 
         protected override Expression VisitParameter(ParameterExpression node)
         {
-            return Expression.Constant(node);
+            var exp = values.ContainsKey(node.Name)
+                ? Expression.Constant(values[node.Name])
+                : base.VisitParameter(node);
+            return exp;
         }
     }
 }
